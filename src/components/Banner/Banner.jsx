69c23717426bd100e42e4799f0e06c65b1883bb8
Mahfuz The Programmer Vetern <mahfuzzayn8@gmail.com>
2023-04-10 15:21:52 +0600

    Added Banner Section with Responsive Styling

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
+import bannerImage from "../../assets/All Images/P3OLGJ1 copy 1.png";
+import "./Banner.css";
 
 const Banner = () => {
     return (
-        <div className='banner-section bg-[#faf8ff]'>
-            
+        <div className="banner-section bg-[#faf8ff] pt-[60px] px-6">
+            <div className="container max-w-[1920px] flex flex-col md:flex-row md:justify-center item-start md:items-end gap-[35px] mx-auto">
+                <div className="content max-w-[570px] md:mb-[136px]">
+                    <h2 className="text-[60px] lg:text-[80px] text-[#1A1919] font-bold lg:leading-[100px]">
+                        One Step Closer To Your{" "}
+                        <span className="linear-text">Dream Job</span>
+                    </h2>
+                    <p className="text-[18px] text-[#757575] font-medium mt-6">
+                        Explore thousands of job opportunities with all the
+                        information you need. Its your future. Come find it.
+                        Manage all your job application from start to finish.
+                    </p>
+                    <button className="bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-[#FFFFFF] text-[20px] font-bold px-[28px] py-[19px] mt-8 rounded-lg">
+                        Get Started
+                    </button>
+                </div>
+                <div className="banner">
+                    <img
+                        className="w-full max-w-[816px] md:min-w-[408px]"
+                        src={bannerImage}
+                        alt=""
+                    />
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,10 @@ import { Link, useLocation } from "react-router-dom";
 import ActiveLink from "../ActiveLink/ActiveLink";
 
 const titles = {
-    "/": "Home",
-    "/about": "About",
+    "/": "Home - JobSearch",
+    "/statistics": "Statistics - JobSearch",
+    "/applied-jobs": "Applied Jobs - JobSearch",
+    "/blog": "Blog - JobSearch",
 };
 
 const Header = () => {
@@ -15,12 +17,12 @@ const Header = () => {
 
     return (
         <header className="header bg-[#faf8ff] pt-[60px] px-6">
-            <div className="container max-w-[1920px] flex justify-around items-center mx-auto">
+            <div className="container max-w-[1920px] flex flex-col md:flex-row md:justify-around items-start md:items-center gap-y-6 mx-auto">
                 <h2 className="text-[32px] text-[#1A1919] font-extrabold">
                     <Link to="/">JobSearch</Link>
                 </h2>
                 <nav>
-                    <ul className="flex gap-x-[42px] text-[#757575] text-[16px] font-medium">
+                    <ul className="flex flex-col sm:flex-row gap-x-[42px] text-[#757575] text-[16px] font-medium">
                         <li>
                             <ActiveLink to="/">Home</ActiveLink>
                         </li>
